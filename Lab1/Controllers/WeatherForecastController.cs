19b2ce01fdc7c81fb08d079f371159ab1288a463
Sebestyen Robert <sebestyenlrobert@gmail.com>
2021-05-15 15:39:45 +0300

    Init project and changing to sqlserver

--- Lab1/Controllers/WeatherForecastController.cs
+++ Lab1/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace Lab1.Controllers
 {
     [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- Lab1/Startup.cs
+++ Lab1/Startup.cs
@@ -27,7 +27,7 @@ namespace Lab1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
